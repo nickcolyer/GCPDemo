7456ad07fde3b1cfa807f76ee2713600f126cd40
Nick Colyer <nickcolyer@gmail.com>
2021-03-25 08:03:51 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "google" {
+  project = "terrademo"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
+
+terraform {
+  backend "remote" {
+    hostname      = "app.terraform.io"
+    organization  = "my-org"
+
+    workspaces {
+      name = "my-repo"
+    }
+  }
+}
